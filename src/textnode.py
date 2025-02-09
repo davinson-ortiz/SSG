ebbd44958f0a1a2159e7a590e528edbab735f2c0
davinson-ortiz <davinson.ortiz.rivas@gmail.com>
2025-02-09 15:42:37 -0500

    Create a TextNode to represent types in line text

---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    NORMAL = "Normal"
+    BOLD = "Bold" 
+    ITALIC = "talic" 
+    CODE = "Code" 
+    LINKS = "Links"
+    IMAGES = "Images"
+    
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+        
+    def __eq__(self, value: TextType) -> bool:
+        if not isinstance(value, TextNode):
+            return False
+        return (self.text == value.text and
+                self.text_type == value.text_type and
+                self.url == value.url)
+        
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
