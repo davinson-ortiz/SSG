1ede40e87a3923d213a296918e1e935351afb805
Davinson <davinson.ortiz.rivas@gmail.com>
2025-03-12 18:02:19 -0500

    ??????????

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -31,6 +31,5 @@ def block_to_block_type(md_block):
     return BlockType.PARAGRAPH
 
 
-
 def markdown_to_blocks(markdown: str)->list[str]:
     return [block.strip() for block in markdown.split("\n\n") if block.strip()]
--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from text_node import TextNode, TextType
-from html_node import LeafNode
+from html_node import LeafNode, ParentNode
+from block_markdown import markdown_to_blocks, block_to_block_type
 
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node) 
-    
+    md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+    blocks = markdown_to_blocks(md)
+    print(blocks)
 
 if __name__ == "__main__":
     main()
