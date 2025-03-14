40a9a27da7eb6ea99d9398e3418dd14d363a38b5
Davinson <davinson.ortiz.rivas@gmail.com>
2025-03-14 17:55:03 -0500

    ??????

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -33,3 +33,4 @@ def block_to_block_type(md_block):
 
 def markdown_to_blocks(markdown: str)->list[str]:
     return [block.strip() for block in markdown.split("\n\n") if block.strip()]
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from text_node import TextNode, TextType
-from html_node import LeafNode, ParentNode
-from block_markdown import markdown_to_blocks, block_to_block_type
+from html_node import LeafNode, ParentNode, HTMLNode
+from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
 
 
 def main():
@@ -14,7 +14,28 @@ This is another paragraph with _italic_ text and `code` here
 """
 
     blocks = markdown_to_blocks(md)
-    print(blocks)
+    html_nodes = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        match block_type:
+            case BlockType.QUOTE:
+                html_nodes.append(HTMLNode("<blockquote>", block))
+            case BlockType.UNORDERED_LIST:
+                html_nodes.append(HTMLNode("<ul>", block))
+            case BlockType.ORDERED_LIST:
+                html_nodes.append(HTMLNode("<ol>", block))
+            case BlockType.CODE:
+                html_nodes.append(HTMLNode("<pre><code>", block))
+            case BlockType.HEADING:
+                html_nodes.append(HTMLNode("<h1>", block))
+            case BlockType.PARAGRAPH:
+                html_nodes.append(HTMLNode("<p>", block))
+    
+    return html_nodes
+
+
+def text_to_children(text):
+    for html
 
 if __name__ == "__main__":
     main()
