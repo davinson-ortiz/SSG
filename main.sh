a36666c61f8de53a95e0194209bbefee182450bf
Davinson <davinson.ortiz.rivas@gmail.com>
2025-03-20 14:44:42 -0500

    to_public functions implemented

--- main.sh
+++ main.sh
@@ -1,1 +1,9 @@
-python3 src/main.py
+#!/bin/bash
+
+if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+  # Windows
+  python src/main.py
+else
+  # Linux/macOS
+  python3 src/main.py
+fi
--- src/main.py
+++ src/main.py
@@ -1,11 +1,30 @@
-from text_node import TextNode, TextType
-from html_node import LeafNode, ParentNode, HTMLNode
-from block_markdown import markdown_to_blocks, block_to_block_type, BlockType
+import os
+from shutil import copy, rmtree
 
+def to_public(src_path: str = "static", dest_path: str = "public"):
+    # Limpiar directorio público antes de copiar (opcional)
+    if os.path.exists(dest_path):
+        rmtree(dest_path)
+    # Crear directorio destino si no existe
+    os.mkdir(dest_path)
+    
+    # Listar elementos en src_path
+    for item in os.listdir(src_path):
+        src_item = os.path.join(src_path, item)
+        dest_item = os.path.join(dest_path, item)
+        
+        if os.path.isfile(src_item):
+            # Copiar archivo
+            copy(src_item, dest_item)
+            print(f"📄 Copiado {src_item} -> {dest_item}")
+        else:
+            # Llamada recursiva para directorios
+            print(f"📂 Procesando directorio: {src_item}")
+            to_public(src_item, dest_item)
 
 def main():
-    pass
+    print("📂 Procesando directorio: static")
+    to_public()
 
-    
 if __name__ == "__main__":
     main()
