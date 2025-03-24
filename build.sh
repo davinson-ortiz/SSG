22a3baf434c5e3c1c2f1a9e5d7b1731d363761e7
davinson-ortiz <davinson.ortiz.rivas@gmail.com>
2025-03-24 11:41:00 -0500

    Fix repository name, and build pages

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
   # Windows
-  python src/main.py "/ssg/"
+  python src/main.py "/SSG/"
 else
   # Linux/macOS
-  python3 src/main.py "/ssg/"
+  python3 src/main.py "/SSG/"
 fi
